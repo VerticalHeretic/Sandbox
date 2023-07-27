616b13b85dcbd1c08ab4702deed7f248a22f8fe7
VerticalHeretic <ls.warss98@gmail.com>
2023-07-27 08:38:43 +0200

    Initial Commit

---
+++ Sandbox/SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxApp.swift
+//  Sandbox
+//
+//  Created by Łukasz Stachnik on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
